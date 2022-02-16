da5263b1d3024c553d770b927e683ea8da5e8043
paul.lungu <paul.lungu@camunda.com>
2022-02-16 09:38:35 -0700

    build fix

--- camunda-platform-spring-boot/src/main/java/com/camunda/poc/starter/CamundaApplication.java
+++ camunda-platform-spring-boot/src/main/java/com/camunda/poc/starter/CamundaApplication.java
@@ -1,6 +1,5 @@
 package com.camunda.poc.starter;
 
-import com.camunda.poc.starter.usecase.renewal.AppConfigProperties;
 //import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages={"com.camunda.poc.starter","com.camunda.poc.starter.usecase.renewal"})
+@SpringBootApplication(scanBasePackages={"com.camunda.poc.starter"})
 @EnableProcessApplication("spring-boot-starter")
-@EnableConfigurationProperties(AppConfigProperties.class)
 //@EnableAdminServer
 public class CamundaApplication {
 
