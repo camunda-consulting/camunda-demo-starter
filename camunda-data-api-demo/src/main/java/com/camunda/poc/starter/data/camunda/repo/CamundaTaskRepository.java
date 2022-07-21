27f68ee3c354336724426de3725c87c894ff92e3
paul.lungu <paul.lungu@camunda.com>
2022-07-20 18:16:57 -0600

    remove camunda-data-api-demo

--- camunda-data-api-demo/src/main/java/com/camunda/poc/starter/data/camunda/repo/CamundaTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.camunda.poc.starter.data.camunda.repo;
-
-
-import com.camunda.poc.starter.data.camunda.entity.CamundaTask;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-@Profile("camunda-custom-entities")
-public interface CamundaTaskRepository extends PagingAndSortingRepository<CamundaTask, Long>{}
