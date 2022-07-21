f100fce984fa5554ba84ea398184010d6c7eecf0
paul.lungu <paul.lungu@camunda.com>
2022-07-20 18:20:22 -0600

    remove camunda-platform-spring-boot

--- camunda-platform-spring-boot/src/main/java/com/camunda/poc/starter/integration/email/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.camunda.poc.starter.integration.email;
-
-public interface EmailService {
-
-    public void sendSimpleMessage(String to, String subject, String text);
-
-    public void sendBccMessage(String to, String from, String bcc, String subject, String text);
-}
